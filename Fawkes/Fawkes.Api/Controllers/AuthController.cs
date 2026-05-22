76e4cda8f418e0ff96bbca3d1b90c206676823d5
Dominik Schindler <dominik.schindler@gmx.de>
2026-05-22 22:33:33 +0300

    Added Auth endpoints (not implemented); Removed demo api

---
+++ Fawkes/Fawkes.Api/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fawkes.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost("login")]
+        public async Task<ActionResult<TokenResponse>> PostLoginAsync(LoginRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<MessageResponse>> PostRegisterAsync(RegisterRequest registerRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("register/{token}")]
+        public async Task<ActionResult<MessageResponse>> PostVerifyEmailAsync(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("refresh")]
+        public async Task<ActionResult<TokenResponse>> PostRefreshAsync(RefreshRequest refreshRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("logout")]
+        public async Task<ActionResult> PostLogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("me")]
+        public async Task<ActionResult<UserResponse>> GetMeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        public class LoginRequest
+        {
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+        }
+
+        public class TokenResponse
+        {
+            public required string AccessToken { get; set; }
+            public required string RefreshToken { get; set; }
+            public int ExpiresIn { get; set; }
+
+        }
+
+        public class RegisterRequest
+        {
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+        }
+
+        public class MessageResponse
+        {
+            public required string Code { get; set; }
+            public required string Message { get; set; }
+        }
+
+        public class RefreshRequest
+        {
+            public required string RefreshToken { get; set; }
+        }
+
+        public class UserResponse
+        {
+            public required Guid Id { get; set; }
+            public required string Email { get; set; }
+        }
+
+
+    }
+}
--- Fawkes/Fawkes.Api/Program.cs
+++ Fawkes/Fawkes.Api/Program.cs
@@ -6,12 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +25,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/status", () => "OK");
+app.MapGet("/version", () => "1.0.0");
 
 app.Run();
